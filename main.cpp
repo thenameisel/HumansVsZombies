a361570ef42f66943d834d786dfe0e0c3eae48f5
thenameisel <thenameiselwisman@gmail.com>
2025-03-26 11:21:11 -0300

    added provided headers

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "Organism.h"
+#include "City.h"
+//#include "
+
+using namespace std;
+
+void ClearScreen()
+{
+    cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+}
+
+int main() {
+    City *city = new City();
+    chrono:: milliseconds interval(INTERVAL);
+
+    while (city->hasDiversity()) { //while both humans and zombies exist
+        this_thread::sleep_for(interval);
+        ClearScreen();
+        city->step();
+        city->reset(); //resets moved flags
+        city->countOrganisms(Z or H goes here);// run once for each type
+        cout << *city; //prints city
+        cout << "GENERATION " << city->getGeneration() << endl;
+        cout << "HUMANS: " << city->countType(HUMAN_CH) << endl;
+        cout << "ZOMBIES: " << city->countType(ZOMBIE_CH) << endl;
+    }
+}
+
